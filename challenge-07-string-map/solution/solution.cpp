e15779e20a88dce534a61834ea1ce78bc392442a
jasonkp <yajie_wan@alumni.brown.edu>
2026-06-13 10:40:58 -0400

    correctly copy strings

--- challenge-07-string-map/solution/solution.cpp
+++ challenge-07-string-map/solution/solution.cpp
@@ -2,6 +2,7 @@
 // This is a correct but slow std::unordered_map reference. You can do MUCH better!
 
 #include "solution.h"
+#include <cstring>
 
 namespace hftu {
 
@@ -25,14 +26,14 @@ void StringMap::insert(const char* key, size_t key_len, uint32_t value) {
     uint64_t hash_mask = 0;
 
     if(key_len > 8){
-        low = reinterpret_cast<uint64_t>(key);
-        raw_high = reinterpret_cast<uint64_t>(key + 8);
+        std::memcpy(&low, key, 8); 
+        std::memcpy(&raw_high, key + 8, key_len - 8); 
         hash_mask = (key_len == 16) ? ~0ULL : (1ULL << (key_len * 8)) - 1;
         high = raw_high & hash_mask;
     }
     else{
         high = 0;
-        raw_low = reinterpret_cast<uint64_t>(key);
+        std::memcpy(&raw_low, key, key_len); 
         hash_mask = (key_len == 8) ? ~0ULL : (1ULL << (key_len * 8)) - 1;
         low = raw_low & hash_mask;
     }
